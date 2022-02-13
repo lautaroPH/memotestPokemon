37421bd493417545c1bdd46c7b553a7f4df74453
lautaroph@gmail.com <lautarph@gmail.com>
2022-02-13 15:43:52 -0300

    add difficulty and localStorage

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,42 @@
+import { useContext } from 'react';
+import { MemoBlockNumber } from '../../context/MemoBlockContext';
+import { VALUES_DIFFICULTY } from '../../helpers/ValuesDifficulty';
 import './Header.css';
 
 const Header = () => {
+  const difficulty =
+    localStorage.getItem('memoBlock') || VALUES_DIFFICULTY.HARD;
+  const { setLimit } = useContext(MemoBlockNumber);
+
+  const handleChange = (e) => {
+    let index = e.target.selectedIndex;
+    const textValue = e.target.options[index].text;
+    const numberValue = e.target.value;
+
+    localStorage.setItem('difficulty', textValue);
+    localStorage.setItem('memoBlock', numberValue);
+    setLimit(numberValue);
+  };
+
   return (
     <div className="containerHeader">
       <h1>Memotest Pokemon</h1>
       <div className="containerInputSelect">
         <p>Dificultad:</p>
-        <select className="inputSelect">
-          <option>FACIL</option>
-          <option>NORMAL</option>
-          <option>DIFICIL</option>
+        <select
+          defaultValue={difficulty}
+          className="inputSelect"
+          onChange={handleChange}
+        >
+          <option label="FACIL" value={VALUES_DIFFICULTY.EASY}>
+            FACIL
+          </option>
+          <option label="NORMAL" value={VALUES_DIFFICULTY.MEDIUEM}>
+            NORMAL
+          </option>
+          <option label="DIFICIL" value={VALUES_DIFFICULTY.HARD}>
+            DIFICIL
+          </option>
         </select>
       </div>
     </div>
---
+++ src/helpers/ValuesDifficulty.js
@@ -0,0 +1,5 @@
+export const VALUES_DIFFICULTY = {
+  EASY: 4, // === 8 FICHAS
+  MEDIUEM: 8, // === 16 FICHAS
+  HARD: 12, // === 24 FICHAS
+};
