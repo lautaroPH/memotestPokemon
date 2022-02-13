8587cdd7cf0a29d43082be39a726c9fe8abbb970
lautaroph@gmail.com <lautarph@gmail.com>
2022-02-13 15:42:18 -0300

    add limitContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import MemotestCards from './components/MemotestCards';
+import { MemoBlockNumber } from './context/MemoBlockContext';
 
 function App() {
+  const limitForMemoBlockInLocalStorage =
+    localStorage.getItem('memoBlock') || '12';
+
+  const [limit, setLimit] = useState(limitForMemoBlockInLocalStorage);
+
   return (
-    <div>
-      <Header />
-      <MemotestCards />
-    </div>
+    <MemoBlockNumber.Provider value={{ limit, setLimit }}>
+      <div>
+        <Header />
+        <MemotestCards />
+      </div>
+    </MemoBlockNumber.Provider>
   );
 }
 
