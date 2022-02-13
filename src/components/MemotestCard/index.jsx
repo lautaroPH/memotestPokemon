3e7cf0baa9c4d8269b37d702b60c708ac7c61b9b
lautaroph@gmail.com <lautarph@gmail.com>
2022-02-12 22:02:46 -0300

    Add funcionality to  compare two memoBlock

--- src/components/MemotestCard/index.jsx
+++ src/components/MemotestCard/index.jsx
@@ -1,17 +1,17 @@
 import { useEffect, useState } from 'react';
 import './MemotestCard.css';
 
-const MemotestCard = ({ flipped, index, name, url }) => {
-  const [pokemon, setPokemon] = useState([]);
-  const pokemonImage = pokemon?.sprites?.other?.home?.front_default;
+const MemotestCard = ({ pokemon, animating, handleMemoClick }) => {
+  const [pokemonImg, setPokemonImg] = useState([]);
+  const pokemonImage = pokemonImg?.sprites?.other?.home?.front_default;
 
   const fetchDataPokemon = async () => {
     try {
-      const response = await fetch(`${url}`);
+      const response = await fetch(`${pokemon.pokemon.url}`);
       const data = await response.json();
-      setPokemon(data);
+      setPokemonImg(data);
     } catch (error) {
-      throw new Error(err);
+      throw new Error(error);
     }
   };
 
@@ -20,8 +20,15 @@ const MemotestCard = ({ flipped, index, name, url }) => {
   }, []);
 
   return (
-    <div className="memo-block">
-      <div className={`memo-block-inner ${flipped && `memo-block-flipped`}`}>
+    <div
+      className="memo-block"
+      onClick={() => !pokemon.flipped && !animating && handleMemoClick(pokemon)}
+    >
+      <div
+        className={`memo-block-inner ${
+          pokemon.flipped && `memo-block-flipped`
+        }`}
+      >
         <div className="memo-block-front">
           <div className="memotestBoxBorderRadiusBig">
             <div className="memotestBoxBorderRadiusMediuem">
@@ -30,7 +37,11 @@ const MemotestCard = ({ flipped, index, name, url }) => {
           </div>
         </div>
         <div className="memo-block-back">
-          <img className="pokemonImage" src={pokemonImage} alt={name} />
+          <img
+            className="pokemonImage"
+            src={pokemonImage}
+            alt={pokemon.pokemon.name}
+          />
         </div>{' '}
       </div>
     </div>
--- src/components/MemotestCards/index.jsx
+++ src/components/MemotestCards/index.jsx
@@ -4,6 +4,8 @@ import './MemotestCards.css';
 
 const MemotestCards = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [selectedMemoBlock, setSelectedMemoBlock] = useState(null);
+  const [animating, setAnimating] = useState(false);
 
   const fetchDataPokemon = async () => {
     const offset = Math.floor(Math.random() * (1100 - 0 + 1) + 0);
@@ -38,16 +40,43 @@ const MemotestCards = () => {
     fetchDataPokemon();
   }, []);
 
+  const handleMemoClick = (pokemonBlock) => {
+    const flippedMemoBlock = { ...pokemonBlock, flipped: true };
+
+    let shuffledMemoBlocksCopy = [...pokemons];
+    shuffledMemoBlocksCopy.splice(pokemonBlock.index, 1, flippedMemoBlock);
+
+    setPokemons(shuffledMemoBlocksCopy);
+
+    if (selectedMemoBlock === null) {
+      setSelectedMemoBlock(pokemonBlock);
+    } else if (selectedMemoBlock.pokemon.name === pokemonBlock.pokemon.name) {
+      setSelectedMemoBlock(null);
+    } else {
+      setAnimating(true);
+      setTimeout(() => {
+        shuffledMemoBlocksCopy.splice(pokemonBlock.index, 1, pokemonBlock);
+        shuffledMemoBlocksCopy.splice(
+          selectedMemoBlock.index,
+          1,
+          selectedMemoBlock
+        );
+        setPokemons(shuffledMemoBlocksCopy);
+        setSelectedMemoBlock(null);
+        setAnimating(false);
+      }, 1000);
+    }
+  };
+
   return (
     <div className="container">
       <div className="memotestContainer">
         {pokemons.map((pokemon) => (
           <MemotestCard
             key={pokemon.index}
-            flipped={pokemon.flipped}
-            index={pokemon.index}
-            name={pokemon.pokemon.name}
-            url={pokemon.pokemon.url}
+            pokemon={pokemon}
+            animating={animating}
+            handleMemoClick={handleMemoClick}
           />
         ))}
       </div>
