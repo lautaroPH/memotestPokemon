e6cb025f087f062313484a176003c6e2184ae26f
lautaroph@gmail.com <lautarph@gmail.com>
2022-02-14 11:08:37 -0300

    delete fetchDataPokemon in memotextCard

--- src/helpers/FetchDataPokemon.js
+++
@@ -1,9 +0,0 @@
-export const fetchDataPokemon = async (url) => {
-  try {
-    const response = await fetch(`${url}`);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
