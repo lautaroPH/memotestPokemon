3c2b632b9d03b3080c7708a443deacdec86d057d
lautaroph@gmail.com <lautarph@gmail.com>
2022-02-17 12:09:59 -0300

    add pokemons.flat

--- src/helpers/FetchDataPokemons.js
+++ src/helpers/FetchDataPokemons.js
@@ -1,17 +1,15 @@
 import { shuffleArray } from './ShuffleArray';
 
 export const fetchDataPokemons = async (limit) => {
-  let arrayPokemon = [];
-  let arrayPokemon2 = [];
   let pokemons = [];
 
   for (let i = 0; i < limit; i++) {
     const offset = Math.floor(Math.random() * (898 - 1 + 1) + 1);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${offset}`);
     const data = await response.json();
-    arrayPokemon[i] = data;
-    arrayPokemon2[i] = data;
-    pokemons = shuffleArray([...arrayPokemon, ...arrayPokemon2]);
+    pokemons[i] = [data, data];
   }
-  return pokemons;
+  const pokemonsFlat = pokemons.flat(Infinity);
+  const pokemonsRandom = shuffleArray(pokemonsFlat);
+  return pokemonsRandom;
 };
